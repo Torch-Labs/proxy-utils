60e8f7f6f4fa9ac8879b6861cfac7163e149f2e4
Sumudu Madhushan <79330461+Madhushan1996@users.noreply.github.com>
2024-02-07 05:55:50 +0530

    add geo node (#12)

---
+++ src/plans/geonode/test/index.spec.ts
@@ -0,0 +1,58 @@
+import { generateGeonodeRotatingProxies, generateGeonodeStickyProxies } from '..';
+import { ProxyFormat } from '../../../@types';
+
+describe('Generate Geonode Proxies', () => {
+  const commonConfig = {
+    host: 'testhost',
+    password: 'testpw',
+    domain: 'test',
+    username: 'testuname',
+  };
+
+  describe('generateGeonodeStickyProxies()', () => {
+    it('should generate a sticky proxy in DEFAULT format', () => {
+      const proxy = generateGeonodeStickyProxies({
+        ...commonConfig,
+        country: 'US',
+        proxyFormat: ProxyFormat.DEFAULT,
+      });
+      console.log(proxy);
+
+      const re = /testhost.test:10000:testuname-country-us-session-.{8}-lifetime-60:testpw/g;
+
+      expect(re.test(proxy)).toBeTruthy();
+    });
+  });
+
+  describe('generateGeonodeRotatingProxies()', () => {
+    it('should generate a rotating proxy in DEFAULT format', () => {
+      const proxy = generateGeonodeRotatingProxies({
+        ...commonConfig,
+        country: 'US',
+        proxyFormat: ProxyFormat.DEFAULT,
+      });
+
+      expect(proxy).toEqual('testhost.test:9000:testuname-country-us:testpw');
+    });
+
+    it('should generate a rotating proxy in FORMAT_1 format', () => {
+      const proxy = generateGeonodeRotatingProxies({
+        ...commonConfig,
+        country: 'US',
+        proxyFormat: ProxyFormat.FORMAT_1,
+      });
+
+      expect(proxy).toEqual('testuname-country-us:testpw:testhost.test:9000');
+    });
+
+    it('should generate a rotating proxy in FORMAT_2 format', () => {
+      const proxy = generateGeonodeRotatingProxies({
+        ...commonConfig,
+        country: 'US',
+        proxyFormat: ProxyFormat.FORMAT_2,
+      });
+
+      expect(proxy).toEqual('testuname-country-us:testpw@testhost.test:9000');
+    });
+  });
+});
