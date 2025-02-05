9a60e20eadd3b32ab3a75e65cbfc8ce9d0fe893a
Sumudu Madhushan <smpawuluwage@gmail.com>
2025-02-05 14:23:34 +0530

    add socks

--- src/plans/packetstream/index.ts
+++ src/plans/packetstream/index.ts
@@ -2,15 +2,21 @@ import { ProxyConfig } from '../../@types';
 import { formatProxyString, randomString } from '../../utils';
 import { formatHostAndPort, formatSsl } from './utils';
 
-const DEFAULT_IPROYAL_PORT = 31112;
-const DEFAULT_IPROYAL_EU_PORT = 31113;
-const DEFAULT_IPROYAL_ASIA_PORT = 31114;
+const DEFAULT_PACKETSTREAM_PORT = 31112;
+const DEFAULT_PACKETSTREAM_EU_PORT = 31113;
+const DEFAULT_PACKETSTREAM_ASIA_PORT = 31114;
+const DEFAULT_PACKETSTREAM_SOCKS_PORT = 31115;
+const DEFAULT_PACKETSTREAM_EU_SOCKS_PORT = 31116;
+const DEFAULT_PACKETSTREAM_ASIA_SOCKS_PORT = 31117;
 
 export const generatePacketstreamStickyProxies = (input: ProxyConfig) => {
   const {
     host,
     euHost,
     asiaHost,
+    socksHost,
+    socksEuHost,
+    socksAsiaHost,
     username,
     password,
     country,
@@ -18,25 +24,41 @@ export const generatePacketstreamStickyProxies = (input: ProxyConfig) => {
     port,
     euPort,
     asiaPort,
+    socksPort,
+    socksEuPort,
+    socksAsiaPort,
     ssl,
     proxyFormat,
     authType,
   } = input;
 
-  const proxyPort = port ?? DEFAULT_IPROYAL_PORT;
-  const proxyEuPort = euPort ?? DEFAULT_IPROYAL_EU_PORT;
-  const proxyAsiaPort = asiaPort ?? DEFAULT_IPROYAL_ASIA_PORT;
+  const proxyPort = port ?? DEFAULT_PACKETSTREAM_PORT;
+  const proxyEuPort = euPort ?? DEFAULT_PACKETSTREAM_EU_PORT;
+  const proxyAsiaPort = asiaPort ?? DEFAULT_PACKETSTREAM_ASIA_PORT;
+  const proxySocksPort = socksPort ?? DEFAULT_PACKETSTREAM_SOCKS_PORT;
+  const proxyEuSocksPort = socksEuPort ?? DEFAULT_PACKETSTREAM_EU_SOCKS_PORT;
+  const proxyAsiaSocksPort = socksAsiaPort ?? DEFAULT_PACKETSTREAM_ASIA_SOCKS_PORT;
+
   const proxyEuHost = euHost ? euHost : `${host}eu`;
   const proxyAsiaHost = asiaHost ? asiaHost : `${host}asia`;
+  const proxySocksEuHost = socksEuHost ? socksEuHost : `${socksHost}eu`;
+  const proxyAsiaSocksHost = socksAsiaHost ? socksAsiaHost : `${socksHost}asia`;
 
   const formattedHostAndConfig = formatHostAndPort({
     host: host,
     euHost: proxyEuHost,
     asiaHost: proxyAsiaHost,
+    socksHost: socksHost,
+    socksEuHost: proxySocksEuHost,
+    socksAsiaHost: proxyAsiaSocksHost,
     port: proxyPort,
     euPort: proxyEuPort,
     asiaPort: proxyAsiaPort,
+    socksPort: proxySocksPort,
+    socksEuPort: proxyEuSocksPort,
+    socksAsiaPort: proxyAsiaSocksPort,
     country: country.toLowerCase(),
+    authType,
   });
 
   let proxyHost = formattedHostAndConfig.host;
@@ -58,6 +80,9 @@ export const generatePacketstreamRotatingProxies = (input: ProxyConfig) => {
     host,
     euHost,
     asiaHost,
+    socksHost,
+    socksEuHost,
+    socksAsiaHost,
     username,
     password,
     country,
@@ -65,25 +90,41 @@ export const generatePacketstreamRotatingProxies = (input: ProxyConfig) => {
     port,
     euPort,
     asiaPort,
+    socksPort,
+    socksEuPort,
+    socksAsiaPort,
     ssl,
     proxyFormat,
     authType,
   } = input;
 
-  const proxyPort = port ?? DEFAULT_IPROYAL_PORT;
-  const proxyEuPort = euPort ?? DEFAULT_IPROYAL_EU_PORT;
-  const proxyAsiaPort = asiaPort ?? DEFAULT_IPROYAL_ASIA_PORT;
+  const proxyPort = port ?? DEFAULT_PACKETSTREAM_PORT;
+  const proxyEuPort = euPort ?? DEFAULT_PACKETSTREAM_EU_PORT;
+  const proxyAsiaPort = asiaPort ?? DEFAULT_PACKETSTREAM_ASIA_PORT;
+  const proxySocksPort = socksPort ?? DEFAULT_PACKETSTREAM_SOCKS_PORT;
+  const proxyEuSocksPort = socksEuPort ?? DEFAULT_PACKETSTREAM_EU_SOCKS_PORT;
+  const proxyAsiaSocksPort = socksAsiaPort ?? DEFAULT_PACKETSTREAM_ASIA_SOCKS_PORT;
+
   const proxyEuHost = euHost ? euHost : `${host}eu`;
   const proxyAsiaHost = asiaHost ? asiaHost : `${host}asia`;
+  const proxySocksEuHost = socksEuHost ? socksEuHost : `${socksHost}eu`;
+  const proxyAsiaSocksHost = socksAsiaHost ? socksAsiaHost : `${socksHost}asia`;
 
   const formattedHostAndConfig = formatHostAndPort({
     host: host,
     euHost: proxyEuHost,
     asiaHost: proxyAsiaHost,
+    socksHost: socksHost,
+    socksEuHost: proxySocksEuHost,
+    socksAsiaHost: proxyAsiaSocksHost,
     port: proxyPort,
     euPort: proxyEuPort,
     asiaPort: proxyAsiaPort,
+    socksPort: proxySocksPort,
+    socksEuPort: proxyEuSocksPort,
+    socksAsiaPort: proxyAsiaSocksPort,
     country: country.toLowerCase(),
+    authType,
   });
 
   let proxyHost = formattedHostAndConfig.host;
